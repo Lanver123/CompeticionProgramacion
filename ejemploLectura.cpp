0f705e6b5fad880a22ad8715365da764ba616b86
GAVILÁN GIL <margagi@EVIRL-011-OK.dsic.cloud>
2020-02-05 14:28:56 +0100

    Ejemplo lectura ints

---
+++ ejemploLectura.cpp
@@ -0,0 +1,27 @@
+#include <sstream>
+#include <iostream>
+#include <fstream>      // std::ifstream
+
+using namespace std;
+
+int main(int argc, char * argv[])
+{
+    std::ifstream myfile("example.txt");
+
+    int inputArray[5];
+
+    string str;
+
+    while(getline(myfile, str))
+    {
+        istringstream ss(str);
+        int num;
+        while(ss >> num)
+        {
+            cout << num << " ";
+        }
+    }
+
+    myfile.close();
+    return 0;
+}

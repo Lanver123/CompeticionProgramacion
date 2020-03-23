f35cca2ebf14251d4e38fc36df3870d7382cb6a4
Lanver123 <menoxzhan@gmail.com>
2020-03-23 12:38:50 +0100

    Carmichael 10006 done

--- Carmichael/carm.py
+++ Carmichael/carm.py
@@ -1,11 +1,36 @@
 import sys
 import math
+import itertools as it
 
+primos = [False] * 65000
 carmichaels = [False]*65000
 
-#Precomputar primos con la criba de erastotenes
-def sieve(xs):
-    return [] if not xs else xs[:1] + sieve([x for x in xs[1:] if x % xs[0] > 0])
+def erat3( ):
+    D = { 9: 3, 25: 5 }
+    yield 2
+    yield 3
+    yield 5
+    MASK= 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0,
+    MODULOS= frozenset( (1, 7, 11, 13, 17, 19, 23, 29) )
+
+    for q in it.compress(
+            it.islice(it.count(7), 0, None, 2),
+            it.cycle(MASK)):
+        p = D.pop(q, None)
+        if p is None:
+            D[q*q] = q
+            yield q
+        else:
+            x = q + 2*p
+            while x in D or (x%30) not in MODULOS:
+                x += 2*p
+            D[x] = p
+
+
+for prime in erat3():
+    if prime > 65000:
+        break
+    primos[prime] = True
 
 def iscarmichael(x):
     if primos[x]:
@@ -15,17 +40,14 @@ def iscarmichael(x):
             return False
     return True
 
-primos = sieve(range(2,65000))
-
-for i in range(65000):
+for i in range(2, 65000):
     carmichaels[i] = iscarmichael(i)
 
-if __name__ == "__main__":
-    while True:
-        num = int(sys.stdin.readline().strip())
-        if num == 0:
-            break
-        if carmichaels[num]:
-            print('The number '+str(num)+' is a Carmichael number.')
-        else:
-            print(str(num)+' is normal.')    
+while True:
+    num = int(sys.stdin.readline().strip())
+    if num == 0:
+        break
+    if carmichaels[num]:
+        print('The number '+str(num)+' is a Carmichael number.')
+    else:
+        print(str(num)+' is normal.')
